7b335471869bcee3a7ef1363402df5cca90df95b
OnanThairu <thairuonan@gmail.com>
2022-08-21 12:30:22 +0300

    Cash register lab.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require "pry"
+
+class CashRegister
+    attr_accessor :total, :discount
+
+    items = []
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity=1)
+        @total += price * quantity
+    end
+
+    def apply_discount
+        if (self.discount != 0)
+            percentage_discount = (100.0 - self.discount.to_f) / 100
+            self.total = (self.total * percentage_discount).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    # def items
+    #     self.items
+    # end
+end
+
+haya = CashRegister.new
 
